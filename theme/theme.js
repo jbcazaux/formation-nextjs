45f905fe14f1ef8ae767c845c89ba44913aa742c
jb cazaux <jbcazaux@gmail.com>
2021-03-27 01:31:19 +0100

    material UI 5 for react 17

--- theme/theme.js
+++ theme/theme.js
@@ -1,16 +1,20 @@
 import { createMuiTheme } from '@material-ui/core/styles'
-import purple from '@material-ui/core/colors/purple'
-import green from '@material-ui/core/colors/green'
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: purple[500],
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
     },
     secondary: {
-      main: green[500],
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
     },
   },
-})
+});
 
 export default theme
